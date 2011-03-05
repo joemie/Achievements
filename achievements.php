94c8fc927dde1fac039633f882818229fdee0ea7
Joseph <joemie92@gmail.com>
2011-03-05 15:28:45 -0800

    need to fix the querys

--- achievements.php
+++ achievements.php
@@ -1,21 +1,77 @@
 <?php
   include 'mysql_connection.php';
-  $value = mysql_real_escape_string($_GET["url"]);
-  //echo $value;
-  //echo "</br>";
   
-// $query = "SELECT * FROM `test` WHERE `url` = '$value'";
-$query = "SELECT * FROM `test`";
- echo "Our query is $query<br />";
-$result = mysql_query($query)
-            or die("invalid query" . mysql_error());
- echo "Number of rows returned: " . mysql_num_rows($result) . "<br />";
-    
- while($row = mysql_fetch_object($result))
- {
-    echo $row->aName;
-    echo "</br>";
- }
+  function getRealIpAddr()
+  {
+	if (!empty($_SERVER['HTTP_CLIENT_IP'])) 
+	{   
+		//check ip from shared internet 
+		$ip = $_SERVER['HTTP_CLIENT_IP'];
+	}
+	elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) 
+    {
+		//to check if ip is passed from proxy
+		$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	} 
+	else 
+	{
+		$ip = $_SERVER['REMOTE_ADDR'];
+	}
+		return $ip;
+  }
+	
+ /* FIIIXX MEEE NOWWW */
+  function incrementCounter()
+  {
+	$query = "UPDATE `progress` SET";
+  }
+  
+  function addUser()
+  {
+	$ip = $_SERVER['REMOTE_ADDR'];
+	$knownIPs = mysql_query("SELECT * FROM `users` WHERE `IP` = '$ip'");
+	$recognizedip = validIP($ip);
+	  
+	if(! $recognizedip )
+	{
+      $query = "INSERT INTO `users` VALUES ('', '$ip', '$level', '$points')";
+	  $write = mysql_query($query);
+	}
+  }
+  function validIP($ip)
+  {
+    $validip = true;
+    while($row = mysql_fetch_object($knownIPs))
+    {
+	  if($row->ip == $ip)  //if the ip is already added
+	  {
+	  	$validip = false;
+	  }
+    }
+	return $validip;
+  }
+  
+  function fetchAchievement()
+  {
+    $url = mysql_real_escape_string($_GET["url"]);
+	
+	$query = "SELECT * FROM `achievements` WHERE `url` = '$url'";
+	$queryResult = mysql_query($query)
+              or die("invalid query" . mysql_error());
+	$numObjects = count($queryResult);
+	
+	$jArray = array();
+	while($row = mysql_fetch_object($queryResult))
+	{
+		$objects = array();
+		$objects["achievementID"] = $row->achievementID;
+		$objects["title"] = $row->title;
+		$objects["description"] = $row->description;
+		$objects["points"] = $row->points;
+		$objects["url"] = $row->url;
+		$jArray[] = $objects;
+	}	
+	return json_encode($jArray);
+  }
+  
 ?>
-
-
